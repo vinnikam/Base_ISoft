f32fd4336568b5b5da12b357686ae0a0307ec53a
vinnikam <vinni9@gmail.com>
2022-09-22 17:40:49 -0500

    Primer commit de aplicación DEMO de Spring Boot

---
+++ src/main/java/com/vinni/demospringboot/DemoSpringApplication.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vinni.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Vinni@
+ */
+@SpringBootApplication
+public class DemoSpringApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio app demo de Spring Boot");
+        SpringApplication.run(DemoSpringApplication.class, args);
+    }
+
+}
